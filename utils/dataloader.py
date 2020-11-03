b1d3857bc33666953ca83d4f7da12469355acc55
JiaqiShi <jiaqishi96@gmail.com>
2020-11-03 22:10:14 +0900

    update

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -1,5 +1,5 @@
-import pickle
 import os
+import pickle
 
 import numpy as np
 import torch
@@ -22,21 +22,38 @@ def get_IEMO_dataloaders(dataset=None, batch_size=2, stream='J'):
 
     return train_loader, valid_loader, test_loader
 
+
 class IEMOCAPDataset(Dataset):
-    def __init__(self, path='/home/shi/git/s-stgcn/data', stream='J', sub_set=0):
+    def __init__(self,
+                 path='/home/shi/git/s-stgcn/data',
+                 stream='J',
+                 sub_set=0):
 
         self.stream = stream
 
-        pkls = ['motion_test.pkl','motion_val.pkl','motion_test.pkl']
+        pkls = ['motion_test.pkl', 'motion_val.pkl', 'motion_test.pkl']
         pkl_path = os.path.join(path, pkls[sub_set])
 
-        with open(pkl_path,'rb') as f:
-            self.ids, self.skeletonData, self.labels = pickle.load(f)
-        
-        if stream is 'B':
-            
+        with open(pkl_path, 'rb') as f:
+            self.ids, self.jointData, self.labels = pickle.load(f)
+
+        if stream is not 'J':
+            self.boneData = {}
+            target_index = [i for i in range(10)]
+            source_index = [0, 0, 1, 2, 2, 4, 5, 2, 7, 8]
+            for k, v in self.jointData:
+                self.boneData[k] = v[:, target_index, :] - v[:, source_index, :]
 
         self.len = len(self.ids)
-    
+
     def __getitem__(self, index):
         key = self.ids[index]
+        if self.stream == 'J':
+            return torch.FloatTensor(self.jointData[key]), torch.tensor(
+                self.labels[key], dtype=torch.long), i
+        elif self.stream == 'B':
+            return torch.FloatTensor(self.boneData[key]), torch.tensor(
+                self.labels[key], dtype=torch.long), i
+        else:
+            return torch.FloatTensor(self.jointData[key]), torch.FloatTensor(self.boneData[key]),\
+                 torch.tensor(self.labels[key], dtype=torch.long), i
