314ce13f70a269af510c7deb2f926e9cc8d19082
JiaqiShi <jiaqishi96@gmail.com>
2020-11-04 20:28:46 +0900

    update

--- models/sstgcn.py
+++ models/sstgcn.py
@@ -1,6 +1,8 @@
+#%%
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
+#%%
 class SSGC(nn.Module):
 
     def __init__(self,
@@ -12,7 +14,7 @@ class SSGC(nn.Module):
                  t_padding=0,
                  t_dilation=1,
                  bias=True,
-                 att=True,
+                 attbranch=True,
                  gate=True,
                  n_head=4,
                  d_kc=0.25,
@@ -21,6 +23,7 @@ class SSGC(nn.Module):
         super().__init__()
 
         self.kernel_size = kernel_size
+        self.attbranch = attbranch
         self.gate = gate
 
         self.bn = nn.BatchNorm2d(in_channels)
@@ -38,13 +41,102 @@ class SSGC(nn.Module):
             dilation=(t_dilation, 1),
             bias=bias)
 
-        if att is True:
-            self.att = MultiHeadSelfAttention(n_head, d_in=in_channels, d_out=out_channels, d_k=d_kc*out_channels, d_v=out_channels*d_vc, residual=True, res_fc=False)
+        if attbranch is True:
+            self.att = SelfAttentionBranch(n_head, d_in=in_channels, d_out=out_channels, d_k=int(d_kc*out_channels), d_v=int(out_channels*d_vc), residual=True, res_fc=False)
 
         if gate is True:
             print('[Info] gate activated.')
             g = nn.Parameter(torch.tensor(1.0, dtype=torch.float32), requires_grad=True)
             self.register_parameter('g', g)
+
+    def forward(self, x, A):
+        inp = self.bn(x)
+
+        f_c = self.conv(inp)
+
+        N, KC, T, V = f_c.size()
+        f_c = f_c.view(N, self.kernel_size, KC//self.kernel_size, T, V)
+        f_c = torch.einsum('nkctv,kvw->nctw', (f_c, A))
+
+        if self.attbranch:
+            N, C, T, V = inp.size()
+            f_a = inp.permute(0, 2, 3, 1).contiguous().view(N*T, V, C)
+            f_a, _ = self.att(f_a, f_a, f_a)
+            f_a = f_a.view(N, T, V, -1).permute(0, 3, 1, 2) # N, C, T, V
+
+            if self.gate:
+                f = (f_a*self.g+f_c)/2
+            else:
+                f = (f_a+f_c)/2
+        else:
+            f = f_c
+
+        return f.contiguous(), A
+
+class SelfAttentionBranch(nn.Module):
+    def __init__(self, n_head, d_in, d_out, d_k, d_v, residual=True, res_fc=False, dropout=0.1, att_dropout=0.1):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_in = d_in
+        self.d_out = d_out
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.residual = residual
+        self.res_fc = res_fc
+
+        self.w_q = nn.Linear(d_in, n_head * d_k, bias=False)
+        self.w_k = nn.Linear(d_in, n_head * d_k, bias=False)
+        self.w_v = nn.Linear(d_in, n_head * d_v, bias=False)
+
+        self.fc = nn.Linear(n_head * d_v, d_out, bias=False)
+
+        self.attention = ScaledAttention(temperature=d_k ** 0.5)
+
+        if residual:
+            self.res = nn.Linear(d_in, d_out) if res_fc or (d_in != d_out) else lambda x: x
+
+        self.dropout = nn.Dropout(dropout)
+        # self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
+        self.att_drop = nn.Dropout(att_dropout)
+
+    def forward(self, q, k, v):
+
+        assert self.d_in == v.size(2)
+
+        # sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+        NT, V, C = v.size()
+
+        if self.residual:
+            res = self.res(v)
+
+        # q = self.layer_norm(q)
+
+        # Pass through the pre-attention projection: b x lq x (n*dv)
+        # Separate different heads: b x lq x n x dv
+        q = self.w_q(q).view(NT, V, self.n_head, self.d_k)
+        k = self.w_k(k).view(NT, V, self.n_head, self.d_k)
+        v = self.w_v(v).view(NT, V, self.n_head, self.d_v)
+
+        # Transpose for attention dot product: b x n x lq x dv
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+
+        att = torch.matmul(q, k.transpose(2, 3))/(self.d_k**0.5)
+        att = self.att_drop(F.softmax(att,dim=3))
+
+        x = torch.matmul(att, v) # NT, H, V, D_v
+
+        x = x.transpose(1,2).view(NT,V,-1)
+        x = self.dropout(self.fc(x))
+
+        if self.residual:
+            x += res
+
+        # Transpose to move the head dimension back: b x lq x n x dv
+        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
+
+        return x, att
 
 class Graph():
     ''' The Graph to model the 3D skeletal data
