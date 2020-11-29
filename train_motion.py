9681cf5fa0a912ccf07822fde9d72e48641ac381
JiaqiShi <jiaqishi96@gmail.com>
2020-11-29 21:16:32 +0900

    update

--- train_motion.py
+++ train_motion.py
@@ -9,13 +9,14 @@ from utils.dataloader import IEMOCAPDataset, get_IEMO_dataloaders
 from utils.train_eval_model import EarlyStopping, Train_Eval_Model
 
 
-def init_seed(seed):
+def init_seed(seed=6):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     # torch.backends.cudnn.enabled = False
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
 
 def import_class(name):
     components = name.split('.')
@@ -24,15 +25,20 @@ def import_class(name):
         mod = getattr(mod, comp)
     return mod
 
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
     else:
-        raise argparse.ArgumentTypeError('[Error] Unsupported value encountered.')
+        raise argparse.ArgumentTypeError(
+            '[Error] Unsupported value encountered.')
+
 
 def main(args):
+
+    init_seed()
 
     epoch_num = args.epoch_num
     class_num = args.class_num
@@ -45,24 +51,33 @@ def main(args):
 
     gate_his = args.gate_his
     stream = args.stream
-    print('[Info] Stream:',stream)
+    print('[Info] Stream:', stream)
 
-    Model = import_class('models.'+args.model)
+    Model = import_class('models.' + args.model)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('[Info] Device: {}'.format(device))
 
-    graph_args = {'strategy':args.graph_strategy,'max_dis_connect':args.max_dis_connect}
+    graph_args = {
+        'strategy': args.graph_strategy,
+        'max_dis_connect': args.max_dis_connect
+    }
 
     if (stream == 'joint') or (stream == 'bone'):
         index = [0]
     elif stream == '2s':
-        index = [0,1]
+        index = [0, 1]
     else:
         raise ValueError('[Error] Stream not existing.')
-    
-    kw = {'attbranch':args.attbranch,'gate':args.gate,'n_head':args.n_head,'d_kc':args.d_kc,'d_vc':args.d_vc}
-    model = Model(3,class_num,graph_args,**kw)
+
+    kw = {
+        'attbranch': args.attbranch,
+        'gate': args.gate,
+        'n_head': args.n_head,
+        'd_kc': args.d_kc,
+        'd_vc': args.d_vc
+    }
+    model = Model(3, class_num, graph_args, **kw)
 
     model_name = model.get_model_name()
 
@@ -72,45 +87,58 @@ def main(args):
     i = 0
     while True:
         if os.path.exists(his_path):
-            i = i+1
+            i = i + 1
             his_path = os.path.join(model_path, model_name + str(i))
         else:
             os.makedirs(his_path)
             print('[Info] History path is {}'.format(his_path))
             break
-    best_model_path = os.path.join(his_path,'model.pkl')
+    best_model_path = os.path.join(his_path, 'model.pkl')
 
-    earlystop = EarlyStopping(patience=patience,save_path=best_model_path, descend_mode=False)
+    earlystop = EarlyStopping(patience=patience,
+                              save_path=best_model_path,
+                              descend_mode=False)
     writer = SummaryWriter(log_dir=his_path)
 
-    dataset = import_class('utils.dataloader.'+args.dataset)
-    train_loader, val_loader, test_loader = get_IEMO_dataloaders(dataset=dataset,batch_size=batch_size,stream=stream)
+    dataset = import_class('utils.dataloader.' + args.dataset)
+    train_loader, val_loader, test_loader = get_IEMO_dataloaders(
+        dataset=dataset, batch_size=batch_size, stream=stream)
 
     if weighted_loss:
-        loss_weights = torch.FloatTensor([
-                                        5492/1102,
-                                        5492/1606,
-                                        5492/1081,
-                                        5492/1703
-                                        ])
+        loss_weights = torch.FloatTensor(
+            [5492 / 1102, 5492 / 1606, 5492 / 1081, 5492 / 1703])
         lossfunc = torch.nn.CrossEntropyLoss(loss_weights.to(device))
     else:
         lossfunc = torch.nn.CrossEntropyLoss()
 
-    train_eval = Train_Eval_Model(model,optim=optimizer,lr=lr,loss_func=lossfunc,device=device,lr_scheduler=lr_scheduler)
-    train_eval.train_model(train_loader,val_loader,test_loader,index,epoch_num,earlystop=earlystop,writer=writer,gate_his=gate_his)
+    train_eval = Train_Eval_Model(model,
+                                  optim=optimizer,
+                                  lr=lr,
+                                  loss_func=lossfunc,
+                                  device=device,
+                                  lr_scheduler=lr_scheduler)
+    _, _, _, _, _, _, labels, preds = train_eval.train_model(
+        train_loader,
+        val_loader,
+        test_loader,
+        index,
+        epoch_num,
+        earlystop=earlystop,
+        writer=writer,
+        gate_his=gate_his)
+
 
 if __name__ == '__main__':
 
     p = argparse.ArgumentParser()
 
-    p.add_argument('--epoch_num', type=int, default=200)
+    p.add_argument('--epoch_num', type=int, default=500)
     p.add_argument('--class_num', type=int, default=4)
     p.add_argument('--patience', type=int, default=50)
     p.add_argument('--batch_size', type=int, default=8)
     p.add_argument('--lr', type=float, default=1e-4)
     p.add_argument('--lr_scheduler', type=str2bool, default=True)
-    p.add_argument('--weighted_loss', type=str2bool, default=True)
+    p.add_argument('--weighted_loss', type=str2bool, default=False)
     p.add_argument('--optimizer', type=str, default='adam')
 
     p.add_argument('--model', type=str, default='sstgcn.SSTGCN')
@@ -126,6 +154,8 @@ if __name__ == '__main__':
     p.add_argument('--n_head', type=int, default=4)
     p.add_argument('--d_kc', type=float, default=0.25)
     p.add_argument('--d_vc', type=float, default=0.25)
+
+    p.add_argument('--comment', type=str, required=False)
 
     args = p.parse_args()
 
